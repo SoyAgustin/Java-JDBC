b18cb33111185d377c282d9c6a7cd8138c1bf6a1
SoyAgustin <Agustinphysx@gmail.com>
2023-08-12 22:29:33 -0600

    Estableciendo conexion y query(statement)SELECT en ProductoController.java

--- Control_Stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ Control_Stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -1,5 +1,9 @@
 package com.alura.jdbc.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,8 +17,24 @@ public class ProductoController {
 		// TODO
 	}
 
-	public List<?> listar() {
-		// TODO
+	//La SQLExcepcion la trata en ControDeStockFrame  en el método cargar tabla con un try catch.
+	public List<?> listar() throws SQLException{
+		//EStablecemos una conexion con la base de datos mysql
+		Connection con = DriverManager.getConnection(
+				"jdbc:mysql://localhost/control_de_stock?useTImeZone=true&serverTimeZone=UTC",
+				"root",
+				"Be594622d2");
+		
+		//En java las querys de sql son del tipo Statement del paquete sql
+		Statement statement = con.createStatement();
+		
+		//SELECT devuelve un true porque devuelve un listado, INSERT, UPDATE O DELETE un false
+		Boolean result = statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM producto");
+		
+		//Vemos en la consola que arroja como resultado un true
+		System.out.println(result);
+		
+		con.close();
 		return new ArrayList<>();
 	}
 
