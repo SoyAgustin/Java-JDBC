33aaf2a3e35bde242f3397e84e55e81f0af4ca47
SoyAgustin <Agustinphysx@gmail.com>
2023-08-19 14:40:12 -0600

    Agregados bloques try with resources en todos los metodos de ProductoController.java

--- Control_Stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ Control_Stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -15,54 +15,60 @@ import com.alura.jdbc.factory.ConnectionFactory;
 public class ProductoController {
 
 	public void modificar(String nombre, String descripcion, Integer cantidad,Integer id) throws SQLException {
-		Connection con = new ConnectionFactory().recuperaConexion();
-		
-		//Haciendo uso de prepared statement para evitar sql inyection
-		PreparedStatement statement = con.prepareStatement("UPDATE producto SET "
-				+ " nombre =?,"
-				+ " descripcion=?,"
-				+ " cantidad=?"
-				+ " WHERE id=?");
-		statement.setString(1,nombre);
-		statement.setString(2,descripcion);
-		statement.setInt(3, cantidad);
-		statement.setInt(4, id);
-		
-		statement.execute();
-		
-		
-		con.close();
+		final Connection con = new ConnectionFactory().recuperaConexion();
+		try(con){
+			//Haciendo uso de prepared statement para evitar sql inyection
+			final PreparedStatement statement = con.prepareStatement("UPDATE producto SET "
+					+ " nombre =?,"
+					+ " descripcion=?,"
+					+ " cantidad=?"
+					+ " WHERE id=?");
+			try(statement){
+				statement.setString(1,nombre);
+				statement.setString(2,descripcion);
+				statement.setInt(3, cantidad);
+				statement.setInt(4, id);	
+				statement.execute();
+			}	
+		}
 	}
 
 	public int eliminar(Integer id) throws SQLException {
-		Connection con  = new ConnectionFactory().recuperaConexion();
-		
-		//Haciendo uso de prepared statement para evitar sql inyection
-		PreparedStatement statement = con.prepareStatement("DELETE FROM producto WHERE ID =?");
-		statement.setInt(1, id);
-		statement.execute();
-		
-		//Para saber si se realizo bien la eliminación usamos el método getUpdateCount()
-		//Este numero entero dice cuántas líneas fueron modificadas.
-		int updateCount = statement.getUpdateCount();
-		
-		con.close();
-		return  updateCount;
+		final Connection con  = new ConnectionFactory().recuperaConexion();
+		try(con){
+			//Haciendo uso de prepared statement para evitar sql inyection
+			final PreparedStatement statement = con.prepareStatement("DELETE FROM producto WHERE ID =?");
+			try(statement){
+				statement.setInt(1, id);
+				statement.execute();
+
+				//Para saber si se realizo bien la eliminación usamos el método getUpdateCount()
+				//Este numero entero dice cuántas líneas fueron modificadas.
+				int updateCount = statement.getUpdateCount();
+
+				con.close();
+
+				return  updateCount;
+			}
+		}
 	}
 
 	//La SQLExcepcion la trata en ControDeStockFrame  en el método cargar tabla con un try catch.
 	//Declaeamos que el metodo devuelve un listado de tipo Map<String,String>
 	public List<Map<String,String>> listar() throws SQLException{
 		//Establecemos una conexion con la base de datos mysql
-		Connection con  = new ConnectionFactory().recuperaConexion();
+		final Connection con  = new ConnectionFactory().recuperaConexion();
+		try(con){
+			//Hacemos uso del prepared statement para evitar sql inyection
+			final PreparedStatement statement = con.prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM producto");
+			try(statement){
+				statement.execute();
+
+				//resultSet es de un tipo iterador, tiene el metodo next
+				ResultSet resultSet = statement.getResultSet();
+			
 		
-		//Hacemos uso del prepared statement para evitar sql inyection
-		PreparedStatement statement = con.prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM producto");
-		statement.execute();
-		
-		//resultSet es de un tipo iterador, tiene el metodo next
-		ResultSet resultSet = statement.getResultSet();
-		
+
 		List<Map<String,String>> resultado  = new ArrayList<>();
 		
 		/*Se itera sobre cada elemento del resultado
@@ -87,54 +93,57 @@ public class ProductoController {
 		}
 		con.close();
 		return resultado;
+			}
+		}
 	}
 
 	
 	
-    public void guardar(Map<String,String> producto) throws SQLException {
-		Connection con = new ConnectionFactory().recuperaConexion();
-		
-		/*Manejo de las transacciones de la base de datos se hacen automaticamente
-		 * */
-		con.setAutoCommit(false);
-		
-		PreparedStatement statement = con.prepareStatement("INSERT INTO producto (nombre, descripcion, cantidad)"+
-		 "VALUES(?,?,?)",Statement.RETURN_GENERATED_KEYS);
-		
-		/*Ahora vamos a agregar una nueva regla de negocio
-		 * solo se admiten en una peticion hasta 50 productos
-		 * si son más la query se dividira en dos*/
-		
-		String nombre = producto.get("NOMBRE");
-		String descripcion = producto.get("DESCRIPCION");
-		Integer cantidad = Integer.valueOf(producto.get( "CANTIDAD"));
-		Integer maximaCantidad = 50;
-		/*La logica va a ser restar la maxima cantidad hasta que la cantidad sea
-		 * cero, de esta forma se dividiran los registros en bloques de la maxima cantidad*/
-		try {	
-			do {
-				int cantidadParaGuardar = Math.min(cantidad, maximaCantidad);
-				ejecutaRegistro(statement, nombre, descripcion, cantidadParaGuardar);
-				cantidad -= maximaCantidad;
-			}while(cantidad>0) ;
-			
-			//Hasta el momento en el que se cumple todo el ciclo (sin errores) se usa el commit
-			con.commit();
-			System.out.println("COMMIT");
-		}catch(Exception e){
-			/*En caso de que haya un error, se cancela la transaccion*/
-			con.rollback();
-			System.out.println("ROLLBACK");
+	public void guardar(Map<String,String> producto) throws SQLException {
+		final Connection con = new ConnectionFactory().recuperaConexion();
+		try (con){
+			/*Manejo de las transacciones de la base de datos se hacen automaticamente
+			 * */
+			con.setAutoCommit(false);
+
+			final PreparedStatement statement = con.prepareStatement("INSERT INTO producto (nombre, descripcion, cantidad)"+
+					"VALUES(?,?,?)",Statement.RETURN_GENERATED_KEYS);
+			try(statement){
+				/*Ahora vamos a agregar una nueva regla de negocio
+				 * solo se admiten en una peticion hasta 50 productos
+				 * si son más la query se dividira en dos*/
+
+				String nombre = producto.get("NOMBRE");
+				String descripcion = producto.get("DESCRIPCION");
+				Integer cantidad = Integer.valueOf(producto.get( "CANTIDAD"));
+				Integer maximaCantidad = 50;
+				/*La logica va a ser restar la maxima cantidad hasta que la cantidad sea
+				 * cero, de esta forma se dividiran los registros en bloques de la maxima cantidad*/
+				try {	
+					do {
+						int cantidadParaGuardar = Math.min(cantidad, maximaCantidad);
+						ejecutaRegistro(statement, nombre, descripcion, cantidadParaGuardar);
+						cantidad -= maximaCantidad;
+					}while(cantidad>0) ;
+
+					//Hasta el momento en el que se cumple todo el ciclo (sin errores) se usa el commit
+					con.commit();
+					System.out.println("COMMIT");
+				}catch(Exception e){
+					/*En caso de que haya un error, se cancela la transaccion*/
+					con.rollback();
+					System.out.println("ROLLBACK");
+				}
+			}
 		}
-		statement.close();
-		con.close();
 	}
 
 	private void ejecutaRegistro(PreparedStatement statement, String nombre, String descripcion, Integer cantidad)
 			throws SQLException {
-	if(cantidad <50) {
-		throw new RuntimeException("Ocurrio un error");
-	}
+//Creando el error de prueba para cantidades menores a 50
+//	if(cantidad <50) {
+//		throw new RuntimeException("Ocurrio un error");
+//	}
 		statement.setString(1, nombre);
 		statement.setString(2, descripcion);
 		statement.setInt(3,cantidad);
@@ -142,14 +151,18 @@ public class ProductoController {
 		/*usando las prepared statement el metodo execute queda vacio*/
 		statement.execute();
 		
-		ResultSet resultSet = statement.getGeneratedKeys();
-		
+		/*Colocando el resultSet dentro del try with resources ya no es necesario 
+		 * cerrar la conexion manualmente, ya que se implementa la intefaz 
+		 * AutoCloseable*/
+		final ResultSet resultSet = statement.getGeneratedKeys();
+		try(resultSet){
 		while(resultSet.next()) {
 			/*Como solo se retorna el id, el metodo getInt busca en la unica 'columna' devuelta*/
 			System.out.println(String.format(
 					"Se guardó el objeto con ID: %d",
 					resultSet.getInt(1) )
 					);
+		}
 		}
 	}
 
