18d5ac022c20545c32c61b18d6878193ce6dfd53
SoyAgustin <Agustinphysx@gmail.com>
2023-08-23 23:03:19 -0600

    Consulta inner join para evitar querys n+1 en listarConProductos() de CategoriaDAO.java

--- Control_Stock/src/main/java/com/alura/jdbc/dao/CategoriaDAO.java
+++ Control_Stock/src/main/java/com/alura/jdbc/dao/CategoriaDAO.java
@@ -26,8 +26,7 @@ public class CategoriaDAO {
 		try { 
 			var querySelect = "SELECT id, nombre FROM categoria";
 			System.out.println(querySelect);
-			final PreparedStatement statement = con.prepareStatement(
-					"SELECT id, nombre FROM categoria") ;
+			final PreparedStatement statement = con.prepareStatement(querySelect) ;
 			try(statement){
 				final ResultSet resultSet = statement.executeQuery();
 				try(resultSet){
@@ -43,6 +42,46 @@ public class CategoriaDAO {
 			throw new RuntimeException(e);
 		}
 		
+	}
+
+	public List<Categoria> listarConProductos() {
+		final Connection con =	new ConnectionFactory().recuperaConexion();
+		List <Categoria> resultado  = new ArrayList<>();
+		
+		try { 
+			/*Se usa el nombre C para la tabla categoria y P para la de producto*/
+			var querySelect = "SELECT C.id, C.nombre, P.id, P.nombre, P.cantidad"
+					+ " FROM categoria C "
+					+" INNER JOIN producto P ON C.id = P.categoria_id";
+			System.out.println(querySelect);
+			final PreparedStatement statement = con.prepareStatement(
+					querySelect) ;
+			try(statement){
+				final ResultSet resultSet = statement.executeQuery();
+				try(resultSet){
+					while(resultSet.next()) {
+						
+						Integer categoriaId = resultSet.getInt("id");
+						String categoriaNombre = resultSet.getString("nombre");
+						
+						var categoria =resultado
+								.stream()
+								.filter(cat -> cat.getId().equals(categoriaId))
+								.findAny().orElseGet(()->{
+									var cat =new Categoria(categoriaId,categoriaNombre );
+									
+									resultado.add(cat);
+									return cat;
+								});
+								
+								
+					}
+					}
+				return resultado;
+				}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
